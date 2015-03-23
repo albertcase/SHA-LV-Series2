ff2589e0aa49a5da82c7a8f0976701f9ea04cf8f
demonzhang <demon.zhang@samesamechina.com>
2015-03-23 21:17:54 +0800

    no message

--- sitereveal/php/videoapi.php
+++ sitereveal/php/videoapi.php
@@ -4,6 +4,6 @@ error_reporting(E_ALL);
 include("ws.php"); 
 $data["act"]="random_video"; 
 $einvitations = wsCall($data);
-echo json_encode($einvitations)
+echo json_encode($einvitations);
 exit;
 ?>

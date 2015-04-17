926373f820ddd4d8c8411489776227a7e3b56596
Spring.Zhou <spring.zhou@samesamechina.com>
2015-04-17 19:56:19 +0800

    change index name to collection name

---
+++ index3.php
@@ -0,0 +1,81 @@
+<?php
+//--start
+require_once 'lib/Mobile-Detect-master/Mobile_Detect.php';
+$detect = new Mobile_Detect;
+
+$deviceType = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
+if($deviceType == 'computer'){
+	//PC redirect
+	$_GET['hmsr'] = isset($_GET['hmsr']) ? "PC-" . $_GET['hmsr'] : "";
+	if($_GET['hmsr']==""){
+		header("Location: /pc/");
+		exit;
+	}
+	header("Location: /pc/?".http_build_query($_GET));
+	exit;
+}
+if($deviceType == 'tablet'){
+	//PC redirect
+	$_GET['hmsr'] = isset($_GET['hmsr']) ? "PAD-" . $_GET['hmsr'] : "";
+	if($_GET['hmsr']==""){
+		header("Location: /pad/");
+		exit;
+	}
+	header("Location: /pad/?".http_build_query($_GET));
+	exit;
+}
+//--end
+?>
+<?php
+	$videos = array('/media/series2/opening2.mp4');
+	$videoindex = array_rand($videos);
+	$myvideo = $videos[$videoindex];
+?>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>路易威登“系列二”线上展览&nbsp;点击进入&nbsp;体验创新数字之旅</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<script src="js/lib/jquery/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />	
+	<style type="text/css">
+		html,body{
+			width: 100%;
+			height: 100%;
+			position: relative;
+			overflow: hidden;
+		}
+	</style>	
+</head>
+<body>
+<img src="/images/share.jpg" class="shareimg" />
+	<video  controls='controls' poster='images/video/placeholder-video.jpg' style="display:block; margin: 10px auto; text-align:center; width:100%;" id="teaser-video">
+		<source src='<?php echo $myvideo; ?>' type='video/mp4' />
+	</video>
+
+	<div class="liens-home home-part" >
+		<a href="index2.php">进入展览</a>
+	</div>
+
+	<a href="http://www.louisvuitton.cn" target="_blank" class="lien-vuitton">www.louisvuitton.cn</a>
+
+<script>
+	$(function(){
+		$(document).ready(function(){
+			$("#teaser-video").height($(window).height()*0.7);
+		});	
+	});
+</script>
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?df4334f8d06255e9fd82b07d21c47e3c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+_hmt.push(['_trackPageview', "series2-uat.php"]);
+</script>
+</body>
+
+</html>

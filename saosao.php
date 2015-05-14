34bd4a94149556c51450244388d478aae2884482
Albert Shen <albertshen1206@gmail.com>
2015-05-14 16:14:29 +0800

    update

---
+++ saosao.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html> 
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>路易威登“系列二”线上展览&nbsp;点击进入&nbsp;体验创新数字之旅</title>
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<link rel="stylesheet" href="css/jquery-mobile.css" /> 
+	<script src="js/lib/jquery/jquery.min.js"></script>
+	<script src="js/lib/jquery/jquery.mobile-1.4.5.js"></script>
+	<script src="js/lib/swipe/jquery.touchSwipe.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<style type="text/css">
+		html,body{
+			width: 100%;
+			height: 100%;
+			position: relative;
+			overflow: hidden;
+		}
+		.saosao{
+			width:100%;
+			display: inline-block;
+		}
+		.logo{
+			width:100%;
+			position: absolute;
+			z-index: 2;
+			top:5%;
+			left:0;
+		}
+
+	</style>
+	<script type="text/javascript" src="js/script.js"></script>
+</head>
+
+<body>
+    <div class="logo">
+    	<img src="sao_logo.jpg" width="100%" />
+    </div>	
+	<div class="saosao">
+		<img src="saosao-1.jpg" width="100%"/>
+	</div>	
+
+	
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?df4334f8d06255e9fd82b07d21c47e3c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+_hmt.push(['_trackPageview', "collection.php"]);
+</script>
+</body>
+</html>
